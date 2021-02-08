085df07d4e9ac72590d072ef202b516b98202040
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-02-08 16:54:47 +0000

    created folder img and moved icons to this plus added svg files for day time and night time

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
     const { cityDetails, weather } = data;
